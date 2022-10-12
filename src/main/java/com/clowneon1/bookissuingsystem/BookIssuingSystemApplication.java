e4dbbaf72a9f088c386b7bdff46ddb278561b49f
yashrajmandloi <er.mandloiyashraj@gmail.com>
2022-10-12 15:03:06 -0400

    init and complete. zenith

--- src/main/java/com/clowneon1/bookissuingsystem/BookIssuingSystemApplication.java
+++ src/main/java/com/clowneon1/bookissuingsystem/BookIssuingSystemApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BookIssuingSystemApplication {
 
+	//Start Api
 	public static void main(String[] args) {
 		SpringApplication.run(BookIssuingSystemApplication.class, args);
 	}
